58f8f5653d7402ff79fbf7517a1c9b65bbd84b66
bimo13 <bimo.jalankaki@yahoo.com>
2014-04-03 14:04:20 +0700

    ImageForm

--- www/includes/js/func-camera.js
+++ www/includes/js/func-camera.js
@@ -1,94 +1,90 @@
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value
+
+// Wait for device API libraries to load
+//
+document.addEventListener("deviceready",onDeviceReady,false);
+
+// device APIs are available
+//
+function onDeviceReady() {
+	pictureSource=navigator.camera.PictureSourceType;
+	destinationType=navigator.camera.DestinationType;
+}
+
 // Called when a photo is successfully retrieved
 //
 function onPhotoDataSuccess(imageData) {
-  // Uncomment to view the base64-encoded image data
-  // console.log(imageData);
-
-  // Get image handle
-  //
-  var smallImage = document.getElementById('smallImage');
-
-  // Unhide image elements
-  //
-  smallImage.style.display = 'block';
-
-  // Show the captured photo
-  // The inline CSS rules are used to resize the image
-  //
-  smallImage.src = "data:image/jpeg;base64," + imageData;
-  
+	// Uncomment to view the base64-encoded image data
+	// console.log(imageData);
+	
+	// Get image handle
+	//
+	var smallImage = document.getElementById('smallImage');
+	
+	// Unhide image elements
+	//
+	smallImage.style.display = 'block';
+	
+	// Show the captured photo
+	// The inline CSS rules are used to resize the image
+	//
+	smallImage.src = "data:image/jpeg;base64," + imageData;
 }
 
 // Called when a photo is successfully retrieved
 //
 function onPhotoURISuccess(imageURI) {
-  // Uncomment to view the image file URI
-  // console.log(imageURI);
-
-  // Get image handle
-  //
-  var largeImage = document.getElementById('largeImage');
-
-  // Unhide image elements
-  //
-  largeImage.style.display = 'block';
-
-  // Show the captured photo
-  // The inline CSS rules are used to resize the image
-  //
-  largeImage.src = imageURI;
+	// Uncomment to view the image file URI
+	// console.log(imageURI);
+	
+	// Get image handle
+	//
+	var largeImage = document.getElementById('largeImage');
+	
+	// Unhide image elements
+	//
+	largeImage.style.display = 'block';
+	
+	// Show the captured photo
+	// The inline CSS rules are used to resize the image
+	//
+	largeImage.src = imageURI;
 }
 
 // A button will call this function
 //
 function capturePhoto() {
-  // Take picture using device camera and retrieve image as base64-encoded string
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-	destinationType: destinationType.DATA_URL });
+	// Take picture using device camera and retrieve image as base64-encoded string
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+		quality: 50,
+		destinationType: destinationType.DATA_URL
+	});
 }
 
 // A button will call this function
 //
 function capturePhotoEdit() {
-  // Take picture using device camera, allow edit, and retrieve image as base64-encoded string
-  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 20, allowEdit: true,
-	destinationType: destinationType.DATA_URL });
+	// Take picture using device camera, allow edit, and retrieve image as base64-encoded string
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+		quality: 20, allowEdit: true,
+		destinationType: destinationType.DATA_URL
+	});
 }
 
 // A button will call this function
 //
 function getPhoto(source) {
-  // Retrieve image file location from specified source
-  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-	destinationType: destinationType.FILE_URI,
-	sourceType: source });
+	// Retrieve image file location from specified source
+	navigator.camera.getPicture(onPhotoURISuccess, onFail, {
+		quality: 50,
+		destinationType: destinationType.FILE_URI,
+		sourceType: source
+	});
 }
 
 // Called if something bad happens.
 //
 function onFail(message) {
-	$("#myDialogs").empty();
-	$("#myDialogs").html("<div class=\"text-danger\">Alert !</div>");
-	
-	$("#myDialogsText").removeClass("alert-success alert-info alert-warning alert-danger");
-	$("#myDialogsText").addClass("alert-danger");
-	$("#myDialogsText").html("Failed, reason : " + message);
-	
-	$("#button-DialogYes").removeClass("btn-info btn-danger btn-warning btn-primary hide");
-	$("#button-DialogNo").removeClass("hide");
-	$("#button-DialogClose").removeClass("hide");
-	
-	$("#button-DialogYes").unbind();
-	$("#button-DialogNo").unbind();
-	$("#button-DialogClose").unbind();
-	
-	$("#button-DialogClose").bind("click", function(){
-		$("#AppDialogs").modal("hide");
-	});
-	
-	$("#button-DialogYes").addClass("hide");
-	$("#button-DialogNo").addClass("hide");
-	
-	$("#pleasewait").modal('hide');
-	$("#AppDialogs").modal();
+	alert('Failed because: ' + message);
 }
