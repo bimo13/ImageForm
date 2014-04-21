1fdd116130a9e68df390f9e9d98d280a8c6995cd
bimo13 <bimo.jalankaki@yahoo.com>
2014-04-21 11:44:58 +0700

    ImageForm

--- www/includes/js/func-createDir.js
+++ www/includes/js/func-createDir.js
@@ -1,42 +1,11 @@
-window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+function createDir(){
+	
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+		fileSystem.root.getDirectory("lightning",{create: true, exclusive: false}, function(dir){alert("Created dir "+dir.name);},function(error){alert("Error creating directory "+ fileErrorCode(error.code));})
+	});
+	
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem) {
+		MyApp.fileSystem.root.getDirectory("lightning/attachments",{create: true, exclusive: false}, function(dir){alert("Created dir "+dir.name);},function(error){alert("Error creating directory "+fileErrorCode(error.code));})
+	});
 
-function gotFS(fileSystem) {
-   fileSystem.root.getDirectory("citylook", {create: true}, gotDir);
-}
-
-function gotDir(dirEntry) {
-    dirEntry.getFile("login.txt", {create: true, exclusive: true}, gotFile);
-}
-
-function gotFile(fileEntry) {
-    // Do something with fileEntry here
-    fileEntry.createWriter(gotFileWriter, fail);
-}
-
-function gotFileWriter(writer) {
-    writer.onwriteend = function(evt) {
-        console.log("contents of file now 'some sample text'");
-        writer.truncate(11);
-        writer.onwriteend = function(evt) {
-            writer.seek(0);
-            writer.write(testo);
-            writer.onwriteend = function(evt){
-                console.log("contents of file now '"+testo+"'");
-            }
-        };
-    };
-    writer.write("some sample text");
-}
-
-function fail(error) {
-    console.log(error.code);
-}
-
-function onDeviceReady() {
-    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, onSuccess, null);
-}
-
-function onSuccess() {
-    fileSystem.root.getDirectory("ABCfolder", {create: true}, gotDir);
-	fileSystem.root.getDirectory("ABCfolder/ABCsubfolder", {create: true}, gotDir);
 }
